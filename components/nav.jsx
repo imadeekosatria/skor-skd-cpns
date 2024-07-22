0f2aadbae41ff16c37be3729f3480aee77da49ae
imadeekosatria <imadeekosatriawiguna@gmail.com>
2024-07-22 14:27:26 +0800

    chore: Add SVG loader, lazy loading for images, and remove unused code and files

---
+++ components/nav.jsx
@@ -0,0 +1,44 @@
+import Link from "next/link"
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+  
+import { Button } from "@/components/ui/button"
+import { Menu } from "lucide-react"
+
+
+const TopNav = ()=>{
+    return (
+        <>
+           <div className="flex w-full flex-col">
+                <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+                    {/* <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm">
+
+                    </nav> */}
+                    <Sheet>
+                        
+                        <SheetTrigger asChild>
+                            <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+                                <Menu className="h-5 w-5"/>
+                                <span className="sr-only">Toggle navigation</span>
+                            </Button>
+                        </SheetTrigger>
+                        <SheetContent side="left">
+                            <nav className="grid mt-4 gap-6 text-lg font-medium">
+                                <Link href="#" className="text-muted-foreground hover:text-foreground">Beranda</Link>
+                            </nav>
+                        </SheetContent>
+                    </Sheet>
+                    <h1 className="text-lg">SKD Sekolah Kedinasan</h1>
+                </header>
+           </div>
+        </>
+    )
+}
+
+export {TopNav}
