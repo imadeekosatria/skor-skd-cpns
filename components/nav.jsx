e9cb20d5c1353882373df2788798ee29552340b5
imadeekosatria <imadeekosatriawiguna@gmail.com>
2024-07-22 15:09:20 +0800

    chore: Add supabase

--- components/nav.jsx
+++ components/nav.jsx
@@ -17,9 +17,6 @@ const TopNav = ()=>{
         <>
            <div className="flex w-full flex-col">
                 <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-                    {/* <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm">
-
-                    </nav> */}
                     <Sheet>
                         
                         <SheetTrigger asChild>
