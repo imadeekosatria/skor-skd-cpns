23f38c69ffbb0d317f043ff7edde5dad9dd157e0
imadeekosatria <imadeekosatriawiguna@gmail.com>
2024-07-22 12:03:52 +0800

    chore: Add SVG loader to webpack config for Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
     output: 'export',
 
+    webpack(config){
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
+    
     images:{
         unoptimized: true,
     }
